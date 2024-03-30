affa5dff2076fee3587fd80a4e6d6a16230a32c9
wzq <1781591279@qq.com>
2024-03-30 16:35:42 +0800

    士大夫

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import functools
 import pandas as pd
 import streamlit as st
 from pages import  detail_page,list_page
@@ -5,12 +6,6 @@ from pages import  detail_page,list_page
 
 from pymongo import MongoClient
 
-table_name='da_xing_jilupian'
-client = MongoClient(host='139.196.158.152', port=27017, username='root', password='1213wzwz', authSource='admin')
-
-db = client.zhiqiang_hot
-tables = db.list_collection_names()
-page_size=10
 
 st.set_page_config(layout="wide")
 st.title("增删改查mongo数据库  + 特定组件组合")
@@ -21,17 +16,28 @@ st.title("增删改查mongo数据库  + 特定组件组合")
 choose_table
 page_number
 page_size
-data
+data_df
+selected_rows
 
 """
 st.session_state['choose_table']=None
-st.session_state['page_number']=None
+st.session_state['page_number']=1
 st.session_state['page_size']=30
-st.session_state['data']={}
+st.session_state['data_df']=pd.DataFrame({})
+st.session_state['selected_rows']=pd.DataFrame({})
 
+
+
+@st.cache_resource
+def get_db():
+    client = MongoClient(host='139.196.158.152', port=27017, username='root', password='1213wzwz', authSource='admin')
+
+    db = client.zhiqiang_hot
+    return db
 
 @st.cache_data
-def get_data_from_mongodb_by_page(table_name,page_number, page_size=page_size):
+def get_data_from_mongodb_by_page(table_name,page_number, page_size=5):
+    db = get_db()
     table = db[table_name]
     skip_count = (page_number - 1) * page_size
     
@@ -45,49 +51,72 @@ def get_data_from_mongodb_by_page(table_name,page_number, page_size=page_size):
 # 数据列表
 def db_tables():
     # 获取数据库中所有表名
+    tables=get_db().list_collection_names()
     with st.sidebar:
         selected_table = st.radio(label='选择表', options=tables)
-
         if selected_table != st.session_state.get('choose_table'):
             st.session_state['choose_table'] = selected_table
+
 
 
 # 列表页
 def list_part():
 
-
+    db=get_db()
     choose_table=st.session_state['choose_table']
-    page_size = st.number_input(label='每页显示数量', min_value=1, step=1, value=st.session_state['page_size'])
-    total_pages= db[choose_table].count_documents({})//page_size+1
-    page_number = st.slider(label='页码', min_value=1, max_value=total_pages, value=1, step=1)
+    total_pages= db[choose_table].count_documents({})//st.session_state['page_size']+1
+
+    col1,col2=st.columns(2)
+    with col1:
+        page_number=st.slider('页码', min_value=1, max_value=total_pages, step=1, value=st.session_state.get('page_number') )
+    with col2:  
+        page_size = st.number_input(label='每页显示数量', min_value=1, step=1, value=st.session_state['page_size'])
+        
     
     # 如果页码发生变化，更新数据
     if page_number != st.session_state.get('page_number') or page_size != st.session_state.get('page_size'):
         st.session_state['page_number'] = page_number
         st.session_state['page_size'] = page_size
     
-    st.session_state['data'] = get_data_from_mongodb_by_page(choose_table, page_number, page_size)
-    editable_df:pd.DataFrame=st.data_editor(st.session_state['data'] )
-    cp_editable_df=editable_df.copy(deep=True)
-
-    # 页面发生了修改
-    if not editable_df.equals(cp_editable_df):
-        diff = cp_editable_df.compare(editable_df)
-        differences = {}
-        for column in diff.columns:
-            changed_rows = diff[diff[column] != 0].index
-            for row in changed_rows:
-                differences[row] = {'editable_df': editable_df.loc[row].to_dict(), 'cp_editable_df': cp_editable_df.loc[row].to_dict()}
-
-        print(differences)
+    # 上面的变动引起重新渲染 导致这里发生了不一致
+    data_df = get_data_from_mongodb_by_page(choose_table, page_number, page_size)
+    if data_df.values!=st.session_state.get('data_df').values:
+        st.session_state['data_df'] = data_df
 
 
+    df_with_selections = data_df.copy()
+    df_with_selections.insert(0, "Select", False)
+    edited_df = st.data_editor(df_with_selections,hide_index=True,
+                               column_config={"Select": st.column_config.CheckboxColumn(required=True),
+                               },
+                               num_rows="Dynamic",
+                               height=800)
 
-
+    selected_rows = edited_df[edited_df.Select]
+    if selected_rows.values!=st.session_state.get('selected_rows').values:
+        st.session_state['selected_rows'] = selected_rows
 
 # 详情页
 def detail_part():
+    # 针对b站的水印进行标注的详情页
+    selected_rows:pd.DataFrame=st.session_state['selected_rows']
+    selected_rows_obj=selected_rows.values.tolist()
+    filed_name=selected_rows.columns.to_list()
+    if len(selected_rows_obj)==0:
+        return
+    if 'bvid' not in filed_name:
+        return
     
+    
+    bvid_indexof=filed_name.index('bvid')
+    bvid=selected_rows_obj[0][bvid_indexof]
+    videod_url=f'http://player.bilibili.com/player.html?bvid={bvid}'
+    iframe_table=f"""<iframe id="my-iframe" src="{videod_url}" width="100%" height="800px" frameborder="0"></iframe>"""
+
+    st.markdown(iframe_table,unsafe_allow_html=True)
+
+    # 图片显示
+
     pass
 
 
